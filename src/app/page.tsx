b068e31ad17c0e94d535207e7aaa7761fc8817a6
S <siddtheone@gmail.com>
2025-08-22 22:33:48 +0530

    Build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import PdfWallpaper from "@/components/PdfWallpaper";
-import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
-
-export const metadata: Metadata = {
-  title: "PDF Wallpaper",
-  description: "PDF Wallpaper by Siddharth",
-};
 
 export default function Home() {
   return <PdfWallpaper />;
